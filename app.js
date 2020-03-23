433bfff4ec0d3e8564587f21fa51f191b6520335
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-03-23 13:00:42 +1100

    GET and POST articles

--- app.js
+++ app.js
@@ -11,13 +11,49 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
+const articleSchema = {
+  title: String,
+  content: String
+};
 
-
-
+const Article = mongoose.model("Article", articleSchema);
 
 
 //TODO
+
+app.get("/articles", function(req, res){
+  Article.find(function(err, foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }else{
+      res.send(err);
+    };  
+  });
+});
+
+app.post("/articles", function(req, res){
+
+  const newArticle = new Article ({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfully added a new article!");
+    }else{
+      res.send(err);
+    };
+  });
+
+});
+
+
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
